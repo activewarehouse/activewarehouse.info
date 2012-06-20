e736e886776dbc262b5311082c9aa75a666203cd
Thibaut Barrère <thibaut.barrere@gmail.com>
2012-06-20 14:04:14 +0200

    Version bump for adapter_extensions

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ class Project
   def self.all
     [
       ['activewarehouse-etl', '1.0.0', '2012-06-20T00:00:00Z', 'A highly hackable Ruby ETL (Extract/Transform/Load) tool. <br/>Tested with MRI 1.9.3, 1.8.7, MySQL and Postgresql.<br/>Requires ActiveRecord 3+.'],
-      ['adapter_extensions', '1.0.0.rc1', '2012-03-03T00:00:00Z', 'ActiveRecord extensions for bulk load and tables manipulations. <br/>Tested with MRI 1.9.3, 1.8.7, MySQL and Postgresql.<br/>Requires ActiveRecord 3+.'], 
+      ['adapter_extensions', '1.0.0', '2012-06-20T00:00:00Z', 'ActiveRecord extensions for bulk load and tables manipulations. <br/>Tested with MRI 1.9.3, 1.8.7, MySQL and Postgresql.<br/>Requires ActiveRecord 3+.'], 
       ['activewarehouse', '0.3.0', '2007-05-13T04:00:00Z', ''], 
       ['rails_sql_views', '0.8.0', '2010-08-25T04:00:00Z', ''], 
     ].map { |data| Project.new(*data) }
